1b111246b7c4fc2087757fd2d02188e1cb8a5f8f
Vitaliy Nishukov <vnishukov@gmail.com>
2019-03-29 20:24:28 +0100

    PaymentsStatusPageComponent spec iteration 1

--- src/app/pages/payments-status-page/payments-status-page.component.spec.ts
+++ src/app/pages/payments-status-page/payments-status-page.component.spec.ts
@@ -1,30 +1,92 @@
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {FormComponent} from 'src/app/components/form/form.component';
-import {GridComponent} from 'src/app/components/grid/grid.component';
-import {PaymentService} from 'src/app/services/payment.service';
+import {HttpClient} from '@angular/common/http';
+import {fakeAsync} from '@angular/core/testing';
+import {Observable, of} from 'rxjs';
+import IPayment from 'src/app/declarations/payment.interface';
 import {PaymentsStatusPageComponent} from './payments-status-page.component';
 
-describe('PaymentsStatusPageComponent', () => {
-  let component: PaymentsStatusPageComponent;
-  let fixture: ComponentFixture<PaymentsStatusPageComponent>;
+class PaymentServiceMock {
+  paymentsStub: IPayment[] = [
+    {
+      id: 1,
+      name: 'P1',
+      daycost: 10,
+      assign: {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+        9: false,
+        10: false,
+        11: false
+      }
+    },
+    {
+      id: 2,
+      name: 'P2',
+      daycost: 20,
+      assign: {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+        9: false,
+        10: false,
+        11: false
+      }
+    }
+  ];
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule],
-      declarations: [PaymentsStatusPageComponent, FormComponent, GridComponent],
-      providers: [PaymentService]
-    }).compileComponents();
-  }));
+  constructor(private httpClient: HttpClient) {}
+
+  getPayments(): Observable<IPayment[]> {
+    return this.httpClient.get<IPayment[]>('url');
+  }
+
+  createPayment(payment: IPayment): Observable<IPayment> {
+    return this.httpClient.post<IPayment>('url', payment);
+  }
+
+  updatePayment(payment: IPayment): Observable<IPayment> {
+    return this.httpClient.put<IPayment>('url', payment);
+  }
+
+  deletePayment(payment: IPayment): Observable<IPayment> {
+    return this.httpClient.delete<IPayment>(`url/:${payment.id}`);
+  }
+}
+
+describe('PaymentsStatusPageComponent', () => {
+  let paymentService: PaymentServiceMock;
+  let component: PaymentsStatusPageComponent;
+  const fakeHttpClient = {
+    get: () => {},
+    post: () => {},
+    put: () => {},
+    delete: () => {}
+  };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PaymentsStatusPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    paymentService = new PaymentServiceMock(fakeHttpClient as any);
+    component = new PaymentsStatusPageComponent(paymentService as any);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch payments on ititialization', fakeAsync(() => {
+    spyOn(fakeHttpClient, 'get').and.returnValue(of(paymentService.paymentsStub));
+    component.ngOnInit();
+    expect(component.payments.length).toEqual(2);
+  }));
 });
